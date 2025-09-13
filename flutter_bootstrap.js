c26852e4d8cb61f398d7db77c009a78179c2a4a7
ketterer-holz <robertklemmer@web.de>
2025-09-13 16:43:21 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d7f80a59fe9bf50f962180e927fa193dc3ac61
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2761313104"
+    serviceWorkerVersion: "3448608338"
   }
 });
